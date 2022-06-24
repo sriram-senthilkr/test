3bfefaf142593d8f00d5eef0bb4177ffd6912c55
sriram-senthilkr <sriram.senthilkr@gmail.com>
2022-06-24 12:54:24 +0530

    commit on mastertest
    Signed-off-by: sriram-senthilkr <sriram.senthilkr@gmail.com>

---
+++ palindrome.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+// using std::vector;
+using namespace std;
+
+bool isPalindrome(int x) {
+    vector<int> old_array;
+    int length=0;   
+    for (int i=0; x>1; i++){
+        old_array.push_back(x %= 10);
+        x = x/10;
+        cout << x;
+        length++;
+    }
+    
+    for (int i=0; i<length; i++){
+        cout << old_array[i]<< endl;
+    }
+
+
+    for (int i=0; i<length; i++){
+        if (old_array[i] != old_array[length - i]){
+            return false;
+        }
+     }
+     return true;
+}
+        
+int main(){
+    int x;
+    cout << "Insert number: ";
+    cin >> x;
+    cout << isPalindrome(x);
+
+}
+        
+
+        
